e880947afe8efa1c5373b69878dc7f320d06c69c
pedrovergueiro <pedrol.vergueiro@gmail.com>
2025-07-02 12:37:04 -0300

    subindo site da faculdade

---
+++ trab.js
@@ -0,0 +1,5 @@
+const botao = document.getElementById("meuBotao");
+
+botao.addEventListener("click", function () {
+  alert("Só não consegue o objetivo quem sonha demasiado. Quem pretende dar o passo maior do que as pernas. Quem acredita que as coisas são fáceis. Todas as coisas são difíceis, elas precisam ser lutadas \n~Silvio Santos");
+});
